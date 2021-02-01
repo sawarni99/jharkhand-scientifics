78dc25277206e88b86c470503c863c9ebcb13680
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-01 16:10:34 +0530

    Created front page file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import FrontPage from "./pages/frontPage/FrontPage";
 
 const App = () => {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <FrontPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/frontPage/FrontPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./FrontPage.css";
+
+export default function FrontPage() {
+  return <div>hello</div>;
+}
