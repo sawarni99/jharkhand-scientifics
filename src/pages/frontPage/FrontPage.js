253c71d14937b6b2fec50347316ba5764f6899c3
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-01 21:53:37 +0530

    Created background

--- src/pages/frontPage/FrontPage.js
+++ src/pages/frontPage/FrontPage.js
@@ -7,8 +7,13 @@ export default function FrontPage() {
     <div className="front-page">
       <Navbar />
       <div className="front-page-background">
+        <div className="front-page-background-title">
+          Get your <span>Science</span> stuffs today
+        </div>
+        <div className="front-page-black" />
         <img src="/images/background3.jpg" alt="" width="100%" height="100%" />
       </div>
+      <div className="front-page-body"></div>
     </div>
   );
 }
