b378122155fe2786795f2a35b41a48b42906e7bf
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-01 21:00:46 +0530

    Added background

--- src/pages/frontPage/FrontPage.js
+++ src/pages/frontPage/FrontPage.js
@@ -4,8 +4,11 @@ import Navbar from "../../components/navbar/Navbar";
 
 export default function FrontPage() {
   return (
-    <div>
+    <div className="front-page">
       <Navbar />
+      <div className="front-page-background">
+        <img src="/images/background3.jpg" alt="" width="100%" height="100%" />
+      </div>
     </div>
   );
 }
