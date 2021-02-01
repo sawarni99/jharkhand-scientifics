ffdcad95bff9fe4eada88d65afe12d54943561f7
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-02 01:52:48 +0530

    Added stocks

--- src/pages/frontPage/FrontPage.js
+++ src/pages/frontPage/FrontPage.js
@@ -3,6 +3,7 @@ import "./FrontPage.css";
 import Navbar from "../../components/navbar/Navbar";
 import ServiceView from "./ServiceView";
 import BrowseView from "./BrowseView";
+import StocksView from "./StocksView";
 import Footer from "../../components/footer/Footer";
 
 export default function FrontPage() {
@@ -13,7 +14,7 @@ export default function FrontPage() {
       <Navbar />
       <div className="front-page-background">
         <div className="front-page-background-title">
-          Get your <span>Science</span> stuffs today
+          Get your <span>Science</span> stuffs today!
         </div>
         <div className="front-page-black" />
         <img src="/images/background3.jpg" alt="" width="100%" height="100%" />
@@ -23,6 +24,8 @@ export default function FrontPage() {
         <ServiceView />
         {divider}
         <BrowseView />
+        {divider}
+        <StocksView />
       </div>
       <div className="front-page-footer">
         <Footer />
---
+++ src/pages/frontPage/StocksView.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./FrontPage.css";
+
+function StocksView() {
+  return (
+    <>
+      <b className="front-page-heading">Stocks</b>
+      <div className="front-page-stock">
+        We deal in Scientific, Instruments, Chemicals, Maps, Charts, Globe and
+        <br />
+        all other requirements of school and colleges.
+        <h1>...</h1>
+        We have stockist of
+        <br /> Omega, Jainco, To-Day Teach, <br /> N.C. Kansil, Blocraft,
+        Brosilicate, Nice, Merk, Loba
+      </div>
+    </>
+  );
+}
+
+export default StocksView;
