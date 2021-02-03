a55646d462bb951ce7960d118a707a79f1f405f4
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-03 18:59:57 +0530

    Created product page layout

--- src/pages/productPage/ProductPage.js
+++ src/pages/productPage/ProductPage.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./ProductPage.css";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 function ProductPage() {
-  return <div></div>;
+  return (
+    <div className="product-page">
+      <div className="product-page-header">
+        <Navbar />
+      </div>
+      <div className="product-page-body"></div>
+      <div className="product-page-footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
