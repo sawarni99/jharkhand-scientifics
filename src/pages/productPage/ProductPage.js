5c80e75e5132d4904c99d9cc965f06efb7bf58c5
Sawarni Swaroop <sawarni99@gmail.com>
2021-02-04 17:26:16 +0530

    Added filters to product page

--- src/pages/productPage/ProductPage.js
+++ src/pages/productPage/ProductPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductPage.css";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
@@ -8,7 +8,14 @@ import divider from "../../utils/divider";
 import Heading from "../../components/heading/Heading";
 
 function ProductPage() {
-  const productView = products.map((data) => (
+  const classNameSelectedOption = "product-page-option-selected";
+  const [optionState, setOptionState] = useState({
+    className: "All",
+    productList: products,
+  });
+  const { className, productList } = optionState;
+
+  const productView = productList.map((data) => (
     <ProductCard
       key={data.key}
       name={data.name}
@@ -16,6 +23,33 @@ function ProductPage() {
       info={data.info}
     />
   ));
+
+  const getProductsByCategory = (category) => {
+    const subProducts = [];
+    for (let product of products) {
+      if (product.category === category) {
+        subProducts.push(product);
+      }
+    }
+    return subProducts;
+  };
+
+  const onClickOptions = (event) => {
+    const category = event.target.id;
+    if (category === "All") {
+      setOptionState({
+        ...optionState,
+        className: "All",
+        productList: products,
+      });
+    } else {
+      setOptionState({
+        ...optionState,
+        className: category,
+        productList: getProductsByCategory(category),
+      });
+    }
+  };
 
   return (
     <div className="product-page">
@@ -25,6 +59,42 @@ function ProductPage() {
       <div className="product-page-body">
         {divider}
         <Heading>Our Products</Heading>
+        <div className="product-page-options">
+          <div
+            className={className === "All" ? classNameSelectedOption : null}
+            id="All"
+            onClick={onClickOptions}
+          >
+            All
+          </div>
+          <div
+            className={
+              className === "Glassware" ? classNameSelectedOption : null
+            }
+            id="Glassware"
+            onClick={onClickOptions}
+          >
+            Glassware
+          </div>
+          <div
+            className={
+              className === "Neutral Glass" ? classNameSelectedOption : null
+            }
+            id="Neutral Glass"
+            onClick={onClickOptions}
+          >
+            Neutral Glass
+          </div>
+          <div
+            className={
+              className === "Chemistry" ? classNameSelectedOption : null
+            }
+            id="Chemistry"
+            onClick={onClickOptions}
+          >
+            Chemistry
+          </div>
+        </div>
         <div className="product-page-content">{productView}</div>
       </div>
       <div className="product-page-footer">
